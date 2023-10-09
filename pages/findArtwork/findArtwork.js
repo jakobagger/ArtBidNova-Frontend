5033b07a65ee7bb1a4cba1cedb61e1c3cf639ead
Selmerr <molybdenum93@gmail.com>
2023-10-09 19:00:40 +0200

    almost working

--- pages/findArtwork/findArtwork.js
+++ pages/findArtwork/findArtwork.js
@@ -5,13 +5,15 @@ const URL = API_URL + "/artwork";
 export async function initFindArtwork() {
     document.querySelector("#btn-id").addEventListener("click", findArtwork);
 }
-    async function findArtwork() {
+
+export async function findArtwork() {
         const id = document.querySelector("#id").value
         
    
     const artwork = await fetch (URL+"/"+id).then(res => res.json())
     const tableRows = `
     <tr>
+    <td style="display:none">${artwork.artworkId}</td>
     <td>${artwork.title}</td>
     <td>${artwork.description}</td>
     <td>${artwork.category}</td>
