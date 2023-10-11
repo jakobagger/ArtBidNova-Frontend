d62724f432eabd17d917d1a422fdd198f9eb2bc8
Selmerr <molybdenum93@gmail.com>
2023-10-11 23:32:26 +0200

    got help

--- pages/findArtwork/findArtwork.js
+++ pages/findArtwork/findArtwork.js
@@ -1,28 +1,36 @@
 import { API_URL } from "../../settings.js";
 import { sanitizeStringWithTableRows } from "../../utils.js";
 const URL = API_URL + "/artwork";
+const REVIEW_URL = API_URL + "/review"
+
 
 export async function initFindArtwork(match) {
 
     document.querySelector("#tablerows").innerHTML = ""
 
-    document.querySelector("#btn-id").addEventListener("click", getArtwork)
+    document.querySelector("#get-reviews").addEventListener("click", fetchAndRenderReviews)
+
+    document.querySelector("#search-form").addEventListener("submit", searchArtwork)
 
     if(match?.params?.id) {
         const id = match.params.id
         document.querySelector("#tablerows").innerHTML = ""
         fetchAndRenderArtwork(id)
     }
-    
+    else {
+        document.querySelector("#get-reviews").classList.add("d-none")
+    }
 }
 
-async function getArtwork(event) {
+async function searchArtwork(event) {
     event.preventDefault()
-    fetchAndRenderArtwork()
+    const searchParams = new URLSearchParams(window.location.search)
+    searchParams.set("id", event.target.idValue.value)
+    window.router.navigate("/find-artwork"+"?" + searchParams.toString())
 }
+
 export async function fetchAndRenderArtwork(idFromURL) {
-        
-    
+
     const id = idFromURL ? idFromURL : document.querySelector("#id").value
         
    try {
@@ -39,10 +47,14 @@ export async function fetchAndRenderArtwork(idFromURL) {
     
 
     document.querySelector("#tablerows").innerHTML = sanitizeStringWithTableRows(tableRows)
-
+    document.querySelector("#get-reviews").classList.remove("d-none")
     return artwork
    } catch (e) {
     console.log("Something fucked up. Good luck")
-   }
+   }   
+}
 
+async function fetchAndRenderReviews() {
+
+    const reviews = await fetch(REVIEW_URL + "/")
 }
