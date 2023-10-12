b4fab5cd700ef4c2769d64f6b7e24b1cfbe30899
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-10-12 08:25:56 +0200

    WIP

--- pages/addAuction/addAuction.js
+++ pages/addAuction/addAuction.js
@@ -2,5 +2,41 @@ import { API_URL } from "../../settings.js";
 import { sanitizeStringWithTableRows } from "../../utils.js";
 
 export async function initaddAuction() {
-    
+    document.querySelector("#add-auction-btn").addEventListener("click", addAuction);
 }
+
+async function addAuction() {
+    const form = document.querySelector("#auction-form");
+          const newAuction = {
+            artworkId: form.artwork-id.value,
+            startDate: form.start-date.value,
+            endDate: form.end-date.value,
+            startBid: form.start-bid.value,
+            minimumIncrement: form.minimum-increment.value
+          };
+  
+          const options = {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(newAuction),
+          };
+  
+          try {
+            const auction = await fetch(URL, options).then((res) => {
+              if (!res.ok) {
+                throw new Error("Auction not added");
+              }
+              return res.json();
+            });
+  
+            document.querySelector("#result").innerText = JSON.stringify(
+              auction,
+              null,
+              3
+            );
+          } catch (e) {
+            document.getElementById("error").innerText = e.message;
+          }
+        };
